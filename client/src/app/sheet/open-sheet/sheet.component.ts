4bbc7243880057fd70e87e9e1f2f8d66b56cd505
Max Yarovatyi <yarovatyimax@gmail.com>
2023-06-06 21:28:53 +0300

    Added component that represents players sheet, added functionality to this component

--- client/src/app/sheet/open-sheet/sheet.component.ts
+++ client/src/app/sheet/open-sheet/sheet.component.ts
@@ -8,23 +8,126 @@ import { SheetService } from '../sheet.service';
   styleUrls: ['./sheet.component.scss'],
 })
 export class SheetComponent implements OnInit {
-  sheets: Sheet[];
+  sheet: Sheet;
+  skillList = [
+    'acrobatics',
+    'animalHandling',
+    'arcana',
+    'athletics',
+    'deception',
+    'history',
+    'insight',
+    'intimidation',
+    'investigation',
+    'medicine',
+    'nature',
+    'perception',
+    'performance',
+    'persuasion',
+    'religion',
+    'sleightOfHand',
+    'stealth',
+    'survival',
+  ];
+  abilityScores = [
+    'strength',
+    'dexterity',
+    'constitution',
+    'intelligence',
+    'wisdom',
+    'charisma',
+  ];
+  skillsWithModifiers = {
+    ['acrobatics']: 'dexterity',
+    ['animalHandling']: 'wisdom',
+    ['arcana']: 'intelligence',
+    ['athletics']: 'strength',
+    ['deception']: 'charisma',
+    ['history']: 'intelligence',
+    ['insight']: 'wisdom',
+    ['intimidation']: 'charisma',
+    ['investigation']: 'intelligence',
+    ['medicine']: 'wisdom',
+    ['nature']: 'intelligence',
+    ['perception']: 'wisdom',
+    ['performance']: 'charisma',
+    ['persuasion']: 'charisma',
+    ['religion']: 'intelligence',
+    ['sleightOfHand']: 'dexterity',
+    ['stealth']: 'dexterity',
+    ['survival']: 'wisdom',
+  };
+  characterProps = [
+    'charName',
 
+    'level',
+
+    'armorClass',
+
+    'playerName',
+
+    'expiriencePoints',
+
+    'initiative',
+
+    'speed',
+
+    'background',
+
+    'alignment',
+
+    'currentHitPoints',
+
+    'maxHitPoints',
+  ];
+  proficiencyBonus: number;
   ngOnInit(): void {
-    this.getSheets();
+    this.getSheet();
   }
 
   constructor(private sheetService: SheetService) {}
 
-  getSheets() {
-    this.sheetService.getSheetById(1).subscribe(
-      (sheets: Sheet[]) => {
-        this.sheets = sheets;
-        console.log(this.sheets);
+  getSheet() {
+    this.sheetService.getSheetById(4).subscribe(
+      (sheet: Sheet) => {
+        this.sheet = sheet;
+        this.proficiencyBonus = Math.floor(this.sheet.level - 1) / 4 + 2;
+        this.fillValues();
+        console.log(this.sheet);
       },
       (error) => {
         console.log(error);
       }
     );
+  }
+  fillValues() {
+    for (let as of this.abilityScores) {
+      document.getElementById(as).innerText = this.sheet.abilityScores[as];
+      const modificator = this.sheet.modificators[as + 'Modificator'];
+      document.getElementById(as + 'Modifier').innerText =
+        modificator > 0 ? '+' + modificator : modificator;
+    }
+    for (let skill of this.skillList) {
+      const returnedSkill = this.sheet.skills[skill];
+      document.getElementById(skill).innerText =
+        returnedSkill > 0 ? '+' + returnedSkill : returnedSkill;
+      const skillIsChecked =
+        this.sheet.skills[skill] - this.proficiencyBonus ==
+        this.sheet.modificators[
+          this.skillsWithModifiers[skill] + 'Modificator'
+        ];
+      if (skillIsChecked)
+        document.getElementById(skill + 'Check').classList.add('checked');
+    }
+    for (let prop of this.characterProps) {
+      document.getElementById(prop).innerText = this.sheet[prop];
+    }
+    document.getElementById('charClass').innerText = this.sheet.charClass.name;
+    document.getElementById('charRace').innerText = this.sheet.charRace.name;
+    document.getElementById('proficiencyBonus').innerText =
+      '+' + this.proficiencyBonus;
+    document.getElementById('passiveWisdom').innerText =
+      '' +
+      (10 + this.sheet.modificators.wisdomModificator + this.proficiencyBonus);
   }
 }
